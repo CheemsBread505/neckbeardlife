f08a408f9ec31a62bd67d570f657771e933e89c0
CheemsBread <dogecheemsbread@gmail.com>
2022-07-24 15:07:28 -0400

    added gifs
    made a page for my gifs

---
+++ copy.js
@@ -0,0 +1,9 @@
+console.log("hello world")
+
+
+function copyText() {
+      
+    /* Copy text into clipboard */
+    navigator.clipboard.writeText
+        ("https://www.neckbeardlife.cf/img/gif/angel_dust_idk1.gif");
+}
